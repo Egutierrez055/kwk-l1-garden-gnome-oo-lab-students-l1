75cdd75b1339c116626735dc2caee9d84ebfe558
Egutierrez055 <egutierrez055@gmail.com>
2018-06-08 20:24:01 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,3 +1,7 @@
 # Code your instances here
 class GardenGnome
-  attr_accessor :ages , :gluten_allergy, :hat_color
+  attr_reader :name ,:ages , :gluten_allergy
+  attr_writer :name ,:ages , :gluten_allergy
+def initialize
+  @personality = evil 
+  
