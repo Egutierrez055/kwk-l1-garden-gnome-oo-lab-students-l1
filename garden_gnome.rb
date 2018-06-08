16f27b9d743a35d05eeadfcd697f9d84953719e8
Egutierrez055 <egutierrez055@gmail.com>
2018-06-08 20:40:14 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -13,16 +13,13 @@ def hat_color (hat_color)
   @hat_color = hat_color
 end
 
-
 def gnaw 
   return "Gnawing on a tree!!!"
 end
 
-
 def shout
   return "GNARLY"
 end 
-
 
 def introduce_self
   return "Hello humans my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
